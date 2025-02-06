626246a705931f904b8fb704bbad83f8e190a6ed
matkogrgic <grgic.matko@gmail.com>
2025-02-06 09:26:38 +0100

    Removed a commented section in main.c

--- DroneStopwatch-copy/Core/Src/main.c
+++ DroneStopwatch-copy/Core/Src/main.c
@@ -97,19 +97,7 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     }
 }
 
-/*
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM2) {
-        if (echo_captured == 0) {
-            echo_start = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-            echo_captured = 1;
-        } else {
-            echo_end = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-            echo_captured = 2;
-        }
-    }
-}
-*/
+
 void readSensor() {
 
 	    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);  // Start the PWM (TRIG pulse)
